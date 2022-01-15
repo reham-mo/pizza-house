dc8500d5f179a9a14220294f6afdb694c19e3d33
Reham <reham.soliman.93@hotmail.com>
2022-01-15 21:04:36 +0200

    first commit

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Pizza;
+
+class PizzaController extends Controller
+{
+
+  public function index() {
+    // get data from a database
+    $pizzas = Pizza::all();
+    // $pizzas = Pizza::orderBy('name')->get();
+    // $pizzas = Pizza::where('type', 'hawaiian')->get();
+
+    return view('pizzas.index', [
+      'pizzas' => $pizzas,
+    ]);
+  }
+
+  public function show($id) {
+    $pizza = Pizza::findOrFail($id);
+    return view('pizzas.show', ['pizza' => $pizza]);
+  }
+
+  public function create(){
+
+    return view('pizzas.create');
+  }
+
+  public function store(){
+
+    $pizza = new Pizza();
+    $pizza->name = request('name');
+    $pizza->type = request('type');
+    $pizza->base = request('base');
+    $pizza->toppings = request('toppings');
+
+    $pizza->save();
+    return redirect('/')->with('mssg', 'Thanks for your order!');
+  }
+
+  public function destroy($id){
+    $pizza = Pizza::findOrFail($id);
+    $pizza->delete();
+    return redirect('/pizzas');
+  }
+
+
+
+}
